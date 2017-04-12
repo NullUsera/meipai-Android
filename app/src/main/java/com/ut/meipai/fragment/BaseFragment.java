e5250ca3c077e3fbc5634d85dfeb1e98ad045afc
renhe <Robin999>
2017-04-12 17:05:56 +0800

    bottomnavgationbar实现底部切换

---
+++ app/src/main/java/com/ut/meipai/fragment/BaseFragment.java
@@ -0,0 +1,20 @@
+package com.ut.meipai.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ut.meipai.R;
+
+
+public class BaseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_test, container, false);
+    }
+}
---
+++ app/src/main/java/com/ut/meipai/fragment/FragmentVideo.java
@@ -0,0 +1,11 @@
+package com.ut.meipai.fragment;
+
+/**
+ * Created by 任和 on 2017/04/12 16:08
+ * Function:  视频列表页
+ * Desc:
+ */
+
+public class FragmentVideo extends BaseFragment {
+
+}
